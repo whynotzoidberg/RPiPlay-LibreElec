af157d337a0f811fd80e0c2623608f592e959a1c
Till Weisser <mail@till-weisser.de>
2021-11-13 21:50:34 +0100

    Try to copy lib

--- RPiPlayBuild/build_with_docker.sh
+++ RPiPlayBuild/build_with_docker.sh
@@ -2,5 +2,5 @@
 rm -rf /storage/RpiPlay
 docker build . -t rpiplay_builder
 mkdir /storage/RpiPlay
-docker run -it -v /storage/RpiPlay:/release rpiplay_builder /bin/bash -c "cp /work/RPiPlay/build/rpiplay /work/RPiPlay/build/lib* /release"
+docker run -it -v /storage/RpiPlay:/release rpiplay_builder /bin/bash -c "cp -r /work/RPiPlay/build/rpiplay /work/RPiPlay/build/lib* /release"
 echo "RPiPlay has been built in the /storage/RpiPlay folder."
